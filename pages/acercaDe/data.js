8662c561074f92d5fb64e5f8307218aa2be3cc57
Leandro Nieto <nleandro0juarg@gmail.com>
2025-07-02 23:52:49 -0300

    Agregado datos Nieto

--- pages/acercaDe/data.js
+++ pages/acercaDe/data.js
@@ -13,4 +13,11 @@ export const integrantes = [
         foto: "https://virtual.unju.edu.ar/pluginfile.php/744082/user/icon/lambda/f1?rev=25287784",
         github: "",
     },
+	{
+        nombre: "Leandro",
+        apellido: "Nieto",
+        rol: "Desarrollador Backend",
+        foto: "https://virtual.unju.edu.ar/pluginfile.php/745504/user/icon/lambda/f1?rev=22095937",
+        github: "https://github.com/yandro899",
+    },
 ];
